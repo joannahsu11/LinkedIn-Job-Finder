7f646fdd630e4021998523ebb9427616065941b3
Joanna Hsu <72953248+joannahsu11@users.noreply.github.com>
2023-03-06 13:28:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#modified date: 3/5/2023
+#
 from tkinter import *
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import sqlite3
 from sqlite3 import Error
 import time
+
 
 def start_running():
     def create_connection(db_file):
@@ -15,22 +18,35 @@ def start_running():
 
         return conn
 
-    def addLine(conn, tablename, results):
-        print(results)
-        sql=f'INSERT INTO "{tablename}" (title,company,"job level",location,"company info",detail) VALUES(?,?,?,?,?,?)'
+    def addLine(conn, tablename, results,keywords):
+        sql = f'INSERT INTO "{tablename}" (title,company,"job level",location,"company info",detail,'
+        for kw in keywords:
+            sql = sql + f'"{kw}",'
+        sql = sql[:-1] + ') VALUES(?,?,?,?,?,?,'
+        for i in range(len(keywords)):
+            sql = sql + f'?,'
+        sql = sql[:-1] + ')'
         cur = conn.cursor()
+        results = sep(results)
         cur.execute(sql, results)
         conn.commit()
         return cur.lastrowid
-
+    
+    def sep(results):
+        skills = results[-1]
+        results = results[:-1]
+        for a in skills.keys():
+            results.append(skills[a])
+        return results
+    
     def getInfo(kws, overview):
-        result = []
+        result = {}
         for keywords in kws:
             s = ''
             for line in overview.split('.'):
                 if keywords.lower() in line.lower():
                     s = s + line + '\n\n'
-            result.append = s
+            result[keywords] = s
         return result
 
     def create_table(conn, create_table_sql):
@@ -40,44 +56,56 @@ def start_running():
         except Error as e:
             print(e)
 
-    def parse(driver,url):
+    def parse(driver, url, keywords):
         title_list = []
         location_list = []
         companyinfo_list = []
         company_list = []
         level_list = []
         detail_list = []
+        kw_list = []
         driver.get(url)
-        left = driver.find_element(By.CSS_SELECTOR,'#main > div > section.scaffold-layout__list>div')
-        for i in range(1,26):
-            print('\t',i)
+        left = driver.find_element(By.CSS_SELECTOR, '#main > div > section.scaffold-layout__list>div')
+        for i in range(1, 3):
             driver.execute_script("arguments[0].scrollTop = arguments[1]", left, 3000)
-            driver.find_element(By.CSS_SELECTOR,f'#main > div > section.scaffold-layout__list > div > ul>li:nth-child({i})').click()
+            driver.find_element(By.CSS_SELECTOR,
+                                f'#main > div > section.scaffold-layout__list > div > ul>li:nth-child({i})').click()
             time.sleep(2)
             try:
-                title = (driver.find_element(By.CSS_SELECTOR,"#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>a>h2").get_attribute('textContent')).lstrip()
+                title = (driver.find_element(By.CSS_SELECTOR,
+                                             "#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>a>h2").get_attribute(
+                    'textContent')).lstrip()
             except:
-                title='N/A'
+                title = 'N/A'
             try:
-                company =(driver.find_element(By.CSS_SELECTOR,"#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div>span>span>a").get_attribute('textContent')).lstrip()
+                company = (driver.find_element(By.CSS_SELECTOR,
+                                               "#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div>span>span>a").get_attribute(
+                    'textContent')).lstrip()
             except:
-                company='N/A'
+                company = 'N/A'
             try:
-                level = ((driver.find_element(By.CSS_SELECTOR,"#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div.mt5.mb2>ul>li>span").get_attribute('textContent')).split(' · ')[1]).lstrip()
+                level = ((driver.find_element(By.CSS_SELECTOR,
+                                              "#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div.mt5.mb2>ul>li>span").get_attribute(
+                    'textContent')).split(' · ')[1]).lstrip()
             except:
-                level='N/A'
+                level = 'N/A'
             try:
-                location = (driver.find_element(By.CSS_SELECTOR,"#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div>span>span.jobs-unified-top-card__bullet").get_attribute('textContent')).lstrip()
+                location = (driver.find_element(By.CSS_SELECTOR,
+                                                "#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div>span>span.jobs-unified-top-card__bullet").get_attribute(
+                    'textContent')).lstrip()
             except:
-                location='N/A'
+                location = 'N/A'
             try:
-                company_info = (driver.find_element(By.CSS_SELECTOR,"#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div.mt5.mb2>ul>li.jobs-unified-top-card__job-insight:nth-child(2)>span").get_attribute('textContent')).lstrip()
+                company_info = (driver.find_element(By.CSS_SELECTOR,
+                                                    "#main > div > section.scaffold-layout__detail.overflow-x-hidden.jobs-search__job-details > div > div.job-view-layout.jobs-details>div>div>div>div>div.relative>div>div.mt5.mb2>ul>li.jobs-unified-top-card__job-insight:nth-child(2)>span").get_attribute(
+                    'textContent')).lstrip()
             except:
-                company_info='N/A'
+                company_info = 'N/A'
             try:
-                detail = (driver.find_element(By.CSS_SELECTOR,"#job-details").get_attribute('textContent')).lstrip()
+                detail = (driver.find_element(By.CSS_SELECTOR, "#job-details").get_attribute('textContent')).lstrip()
             except:
-                detail='N/A'
+                detail = 'N/A'
+            kw_output = getInfo(keywords, detail)
 
             title_list.append(title)
             location_list.append(location)
@@ -85,9 +113,8 @@ def start_running():
             company_list.append(company)
             level_list.append(level)
             detail_list.append(detail)
-
-
-        return title_list, location_list, companyinfo_list, detail_list, company_list, level_list
+            kw_list.append(kw_output)
+        return title_list, location_list, companyinfo_list, detail_list, company_list, level_list, kw_list
 
     def getkw(strings):
         return strings.split(',')
@@ -108,7 +135,7 @@ def start_running():
     detail_list = []
     op = webdriver.ChromeOptions()
     op.add_argument('headless')
-    chromedriver = '{your chromedriver directory'
+    chromedriver = r'chromedriver.exe'
     driver = webdriver.Chrome(executable_path=chromedriver, options=op)
     start = 0
     driver.get("https://linkedin.com/uas/login")
@@ -141,7 +168,7 @@ def start_running():
                 'textContent')
 
 
-            title, location, companyinfo, detail, company, level = parse(driver,url)
+            title, location, companyinfo, detail, company, level, kw = parse(driver,url,getkw(keywords.get()))
 
             title_list = title_list+title
             company_list = company_list+company
@@ -149,9 +176,9 @@ def start_running():
             location_list = location_list+location
             companyinfo_list = companyinfo_list+companyinfo
             detail_list = detail_list+detail
-
+            kw_list = kw_list + kw
         for i in range(len(title_list)):
-            addLine(conn,table.get(),[title_list[i], company_list[i], level_list[i], location_list[i], companyinfo_list[i], detail_list[i]])
+            addLine(conn,table.get(),[title_list[i], company_list[i], level_list[i], location_list[i], companyinfo_list[i], detail_list[i],kw_list[i]],getkw(keywords.get()))
     root.destroy()
 
 root = Tk()
@@ -185,3 +212,5 @@ keywords=Entry(root,width=40)
 keywords.place(x=140,y=160)
 
 root.mainloop()
+
+
